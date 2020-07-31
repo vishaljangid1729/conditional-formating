5d663481930617f99bd06d6e1c30f5e8c9ee8a8a
vishal jangid <jangidvishal1999@gmail.com>
2020-07-31 16:07:02 +0530

    Data Fatched from server.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const App = ()=>{
+const App = () => {
+  const [data, setData] = useState([])
+
+  // Fetching data from server
+
+  useEffect(() => {
+    ;(async () => {
+      const result = await axios('https://api.tvmaze.com/search/shows?q=snow')
+      setData(result)
+    })()
+  }, [])
+
   return (
-    <h1>Hello Univsere</h1>
+    <h1>Hello Universe!</h1>
   )
 }
+
+
+export default App;
